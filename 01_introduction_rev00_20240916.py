776170c30cc05c297f188809b16c95225fac7389
Hyunseung Yoo <hyunseung.yoo.hs@gmail.com>
2024-09-16 20:54:27 +0900

    Update 01_introduction_rev00_20240916.py

--- 01_introduction_rev00_20240916.py
+++ 01_introduction_rev00_20240916.py
@@ -1,15 +1,16 @@
 #
 # TITLE: Tkinter (introduction)
 # AUTHOR: Hyunseung Yoo
-# PURPOSE:  
+# PURPOSE:
 # REVISION:
 # REFERENCE: https://www.youtube.com/watch?v=0zrq2N2qAVo&list=PLnT2pATp7adWUaMh0A8jfoN_OHGAmt_m4
 #
 #
 
 
+# modules
 from tkinter import *
-
+from PIL import ImageTk, Image
 
 # creating window
 root = Tk()
@@ -18,7 +19,7 @@ root = Tk()
 root.title('FEM')
 
 # window size
-root.geometry('1600x800')
+root.geometry('1800x800')
 
 # labels
 label1 = Label(root, text='Finite Element Method', font=('Verdana, 20'))
@@ -57,9 +58,72 @@ print(value)
 
 # check box
 
+def check_function(var):
+    print(var)
 
+varCheck = StringVar()
+varCheck.set('on')
+
+checkbox = Checkbutton(root, text='switch', onvalue='on', offvalue='off',
+                       variable=varCheck, command=lambda: check_function(varCheck.get()),
+                       font=('Verdana', 30))
+checkbox.grid(row=1, column=1)
+
+# sliders
+
+def scaler(val):
+    global slider
+    global sliderentry
+
+    sliderentry.configure(state='normal')
+    sliderentry.delete(0, END)
+    sliderentry.insert(0, f"{slider.get():.2f}")
+    sliderentry.configure(state='readonly')
+
+sliderframe = LabelFrame(root)
+sliderframe.grid(row=0, column=3, padx=30, pady=30)
+
+slider = Scale(sliderframe, from_=-20, to=20, orient='horizontal',
+               width=20, length=200, showvalue=0,
+               command=scaler)
+slider.grid(row=0, column=0)
+
+sliderentry = Entry(sliderframe, width=5, borderwidth=5, font=('Verdana', 20),
+                    justify='center',state='readonly')
+sliderentry.grid(row=1, column=0)
+
+sliderentry.configure(state='normal')
+sliderentry.insert(0, 1.00)
+sliderentry.configure(state='readonly')
+
+# radiobuttons
+
+radioframe = LabelFrame(root)
+radioframe.grid(row=2, column=1, padx=30, pady=30)
+
+colormapVar = StringVar()
+colormapVar.set('jet')
+
+radioJet = Radiobutton(radioframe, variable=colormapVar,text='Jet', value='jet', font=('Verdana', 10))
+radioCopper = Radiobutton(radioframe, variable=colormapVar,text='Copper', value='copper', font=('Verdana', 10))
+radioCool = Radiobutton(radioframe, variable=colormapVar,text='Cool', value='cool', font=('Verdana', 10))
+
+radioJet.grid(row=0, column=0)
+radioCopper.grid(row=1, column=0)
+radioCool.grid(row=2, column=0)
+
+imageJet = Image.new('RGB', (100, 100), color=(0,0,200))
+imageCopper = Image.new('RGB', (100, 100), color=(150, 0, 0))
+imageCool = Image.new('RGB', (100, 100), color=(0,100,100))
+
+imageJet = ImageTk.PhotoImage(imageJet.resize((100, 20)))
+imageCopper = ImageTk.PhotoImage(imageCopper.resize((100, 20)))
+imageCool = ImageTk.PhotoImage(imageCool.resize((100, 20)))
+
+radioJet.configure(image=imageJet)
+radioCopper.configure(image=imageCopper)
+radioCool.configure(image=imageCool)
 
 #
 root.mainloop()
-
 
